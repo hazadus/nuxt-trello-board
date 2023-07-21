5ba3bd96e8dbef0c9e2871c3e477488b4ea34c28
hazadus <hazadus7@gmail.com>
2023-07-22 00:29:27 +0300

    dev: Implemented server auth middleware.

---
+++ server/middleware/auth.ts
@@ -0,0 +1,23 @@
+import { AuthTokenModel } from "@/server/models/AuthToken";
+
+export default defineEventHandler(async (event) => {
+  //
+  // - Check `authorization` header in the request
+  // - Try to find provided token in the database
+  // - If succeeded, set auth flag to `true` in the context, and put user info there.
+  //
+  event.context.isAuthorized = false;
+  event.context.user = null;
+
+  if (event.node.req.headers.authorization) {
+    // Find auth token in the database to ensure it is valid one
+    const authToken = await AuthTokenModel.where({ token: event.node.req.headers.authorization })
+      .findOne()
+      .populate("user", "-hashedPassword");
+
+    if (authToken) {
+      event.context.isAuthorized = true;
+      event.context.user = authToken.user;
+    }
+  }
+});
