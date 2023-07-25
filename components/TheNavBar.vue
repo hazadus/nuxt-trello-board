a51a7dcab9223f5568372a20d907f12433ed7150
hazadus <hazadus7@gmail.com>
2023-07-25 21:37:51 +0300

    dev: Login page implemented.

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -38,6 +38,13 @@ const authStore = useAuthStore();
             <MenuItems
               class="absolute right-0 origin-top-right mt-2 bg-white rounded-md shadow-lg border w-48 focus:outline-none divide-y divide-gray-100">
               <div class="px-1 py-1">
+                <MenuItem v-slot="{ disabled }" disabled>
+                <a href="#" :class="{ 'opacity-40': disabled }" class="block px-4 py-3 text-sm text-gray-700">
+                  {{ authStore.user?.email }}
+                </a>
+                </MenuItem>
+              </div>
+              <div class="px-1 py-1">
                 <MenuItem v-slot="{ active }">
                 <a href="#" :class="{ 'bg-gray-100': active }" class="block px-4 py-3 text-md text-gray-700">Profile</a>
                 </MenuItem>
--- pages/login.vue
+++ pages/login.vue
@@ -1,23 +1,83 @@
 <script setup lang="ts">
-
 const authStore = useAuthStore();
+const router = useRouter();
 
-async function onClickLogin() {
+const emailInputElement: Ref<HTMLInputElement | null> = ref(null);
+
+const email: Ref<string> = ref("");
+const password: Ref<string> = ref("");
+
+const isFetching: Ref<boolean> = ref(false);
+
+async function submitForm() {
+  isFetching.value = true;
+
   await authStore.logIn({
-    email: "myxomop@hazadus.ru",
-    plainPassword: "12345678",
-  })
+    email: email.value,
+    plainPassword: password.value,
+  });
+
+  isFetching.value = false;
+  if (authStore.isAuthenticated) router.push("/");
 }
+
+onBeforeMount(() => {
+  if (authStore.isAuthenticated) {
+    router.push("/");
+  }
+});
+
+onMounted(() => {
+  emailInputElement.value?.focus();
+});
 </script>
 
 <template>
   <Title>
-    Log In
+    Log In | Доскач
   </Title>
 
-  <div class="p-20">
-    <a @click="onClickLogin" class="text-white p-3 inline-block bg-blue-500 cursor-pointer">
-      Login
-    </a>
-  </div>
+  <section class="bg-gray-50">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0" style="height: calc(100vh - 64px);">
+      <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+            Sign in to your account
+          </h1>
+
+          <form class="space-y-4 md:space-y-6" @submit.prevent="submitForm">
+            <div>
+              <label for="email" class="block mb-2 text-sm font-medium text-gray-900">
+                Your email
+              </label>
+              <input v-model="email" ref="emailInputElement" :disabled="isFetching" type="email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                placeholder="name@domain.ru" required>
+            </div>
+
+            <div>
+              <label for="password" class="block mb-2 text-sm font-medium text-gray-900">
+                Password
+              </label>
+              <input v-model="password" :disabled="isFetching" type="password" placeholder="••••••••"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                required>
+            </div>
+
+            <button :disabled="isFetching" :class="isFetching ? 'is-loading' : ''" type="submit"
+              class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+              Sign in
+            </button>
+
+            <p class="text-sm font-light text-gray-500">
+              Don’t have an account yet?
+              <RouterLink to="/signup/" class="font-medium text-primary-600 hover:underline">
+                Sign up
+              </RouterLink>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
