1563b92ef386aab26df509654f47f274ca0832bf
hazadus <hazadus7@gmail.com>
2023-07-25 18:21:07 +0300

    ui: Navbar - added buttons; authd/not authd logic.

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,19 +1,33 @@
 <script setup lang="ts">
-import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue"
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+
+const authStore = useAuthStore();
 </script>
 
 <template>
   <nav class="sticky top-0 bg-teal-600 border-gray-200 shadow-md">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-3">
-      <RouterLink to="/" class="flex items-baseline">
-        <span class="self-center text-2xl font-semibold text-gray-300 whitespace-nowrap">
-          <Icon name="material-symbols:space-dashboard" class="text-3xl" />
+      <div class="flex items-middle">
+        <Icon name="material-symbols:space-dashboard" class="text-3xl mr-2 text-gray-700 drop-shadow-lg" />
+        <RouterLink to="/" class="text-2xl mr-10 font-semibold text-gray-300 whitespace-nowrap drop-shadow-lg"
+          style="line-height: 1.8rem;">
           Доскач
-        </span>
-      </RouterLink>
+        </RouterLink>
+        <button
+          class="navbar-button hidden md:block py-1 px-3 mr-2 text-gray-100 hover:text-white text-sm bg-teal-500 shadow-md rounded-sm">
+          <Icon name="material-symbols:dashboard" />
+          Boards
+        </button>
+        <button
+          class="navbar-button hidden md:block py-1 px-3 text-gray-100 hover:text-white text-sm bg-teal-500 shadow-md rounded-sm">
+          <Icon name="material-symbols:dashboard-customize-outline" />
+          Create
+        </button>
+      </div>
+
       <div class="flex items-center md:order-2">
         <!-- Dropdown user menu -->
-        <Menu as="div" class="relative">
+        <Menu v-if="authStore.isAuthenticated" as="div" class="relative">
           <MenuButton class="focus:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-white rounded-full">
             <img class="w-10 h-10 inline rounded-full" src="/images/default-profile-pic.jpg" alt="Profile image">
           </MenuButton>
@@ -57,40 +71,50 @@ import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue"
           </svg>
         </button>
       </div>
-      <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-        <ul
-          class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
-          <li>
-            <RouterLink to="/" class="block py-2 pl-3 pr-4 text-gray-300 rounded bg-transparent md:p-0">
-              Home
-            </RouterLink>
-          </li>
-          <li>
-            <RouterLink to="/signup/"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-200 md:p-0">
-              Sign Up
-            </RouterLink>
-          </li>
-          <li>
-            <RouterLink to="/login/"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-200 md:p-0">
-              Log In
-            </RouterLink>
-          </li>
-          <li>
-            <RouterLink to="/about/"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-200 md:p-0">
-              About
-            </RouterLink>
-          </li>
-          <li>
-            <RouterLink to="/contact/"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-200 md:p-0">
-              Contact
-            </RouterLink>
-          </li>
-        </ul>
-      </div>
+
+      <template v-if="!authStore.isAuthenticated">
+        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
+          <ul
+            class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
+            <li>
+              <RouterLink to="/" class="block py-2 pl-3 pr-4 text-gray-300 rounded bg-transparent md:p-0">
+                Home
+              </RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/signup/"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-200 md:p-0">
+                Sign Up
+              </RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/login/"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-200 md:p-0">
+                Log In
+              </RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/about/"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-200 md:p-0">
+                About
+              </RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/contact/"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-200 md:p-0">
+                Contact
+              </RouterLink>
+            </li>
+          </ul>
+        </div>
+      </template>
     </div>
   </nav>
 </template>
+
+<style scoped>
+.navbar-button:hover {
+  transform: scale(1.02);
+  transition: all 0.2s;
+}
+</style>
