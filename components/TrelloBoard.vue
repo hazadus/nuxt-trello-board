37af7c84fd847071f79eceb421e0f561a516727e
hazadus <hazadus7@gmail.com>
2023-07-26 14:39:50 +0300

    dev: Created basic layout for Boards list page.

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -114,7 +114,7 @@ onMounted(async () => {
   <template v-if="board">
     <div class="flex justify-between items-middle px-5 py-4">
       <input v-model="board.title"
-        class="text-4xl font-semibold bg-transparent border-none rounded px-1 flex-grow text-gray-100 focus:text-black focus:bg-white  focus:outline focus:outline-gray-400 focus:outline-1"
+        class="text-4xl font-semibold bg-transparent border-none rounded px-1 flex-grow text-gray-100 focus:text-black focus:bg-white  focus:outline focus:outline-gray-400 focus:outline-1 hover:bg-gray-100 hover:bg-opacity-20"
         @keyup.enter="onBoardChange(); ($event.target as HTMLInputElement).blur()" type="text">
 
       <button class="text-gray-100 bg-transparent px-5 py-0 rounded hover:bg-gray-100 hover:bg-opacity-20">
--- pages/boards/index.vue
+++ pages/boards/index.vue
@@ -17,11 +17,28 @@ onBeforeMount(() => {
     All Boards | Доскач
   </Title>
 
-  <ul v-if="boardStore.boards.length" class="p-4">
-    <li v-for="board in boardStore.boards" :key="`board-id-${board._id}`">
-      <RouterLink :to="buildBoardUrl(board._id!)" class="hover:underline">
-        {{ board.title }}
-      </RouterLink>
-    </li>
-  </ul>
+  <h1 class="text-4xl font-semibold text-gray-100 px-5 py-6">
+    All boards
+  </h1>
+
+  <section v-if="boardStore.boards.length"
+    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mb-4 px-5 pt-0 pb-5">
+    <div class="h-48 p-4 bg-gray-50 rounded-lg shadow-md overflow-hidden cursor-pointer text-center hover:bg-opacity-90">
+      Create new board
+    </div>
+
+    <div v-for="board in boardStore.boards" :key="`board-id-${board._id}`"
+      class="p-4 bg-teal-600 rounded-lg shadow-md overflow-hidden cursor-pointer hover:bg-opacity-90"
+      @click="$router.push(buildBoardUrl(board._id!))">
+      <div class="flex flex-col h-full">
+        <div class="flex grow text-2xl text-gray-50 ">
+          {{ board.title }}
+        </div>
+        <div class="grow-0 text-sm text-gray-300">
+          {{ board.columns.length }} columns &middot;
+          {{ board.columns.reduce((accumulator, column) => accumulator + column.tasks.length, 0) }} cards
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
