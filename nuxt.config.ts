2861a885fa80f5e6fa8be3bc810bc876110c3bab
hazadus <hazadus7@gmail.com>
2023-08-04 17:36:29 +0300

    config: @nuxt/devtools installed to project.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: {
+    enabled: true,
+  },
   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "nuxt-icon", "@pinia/nuxt"],
   ssr: false,
   runtimeConfig: {
