915cadfda21f39c736155665dc57501f1ca65cf8
hazadus <hazadus7@gmail.com>
2023-08-17 17:23:02 +0300

    ui: Hamburger button now working on small screens.

--- components/Navbar/Navbar.vue
+++ components/Navbar/Navbar.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 const authStore = useAuthStore();
 
+const isHamburgerOpen = ref(false);
+
 const activeMenuClasses = "block py-2 pl-3 pr-4 text-gray-100 underline rounded bg-transparent md:p-0";
 const menuClasses = "block py-2 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0";
 
@@ -44,7 +46,8 @@ const menu = computed((): IMenuItem[] => [
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-3">
       <div class="flex items-middle">
         <Icon name="material-symbols:space-dashboard" class="text-3xl mr-2 text-gray-700 drop-shadow-lg" />
-        <RouterLink to="/" class="text-2xl mr-10 font-semibold text-gray-300 whitespace-nowrap drop-shadow-lg"
+        <RouterLink to="/"
+          class="text-xl mr-10 font-semibold text-gray-300 whitespace-nowrap drop-shadow-lg hidden md:block"
           style="line-height: 1.8rem;">
           Трололо
         </RouterLink>
@@ -61,25 +64,26 @@ const menu = computed((): IMenuItem[] => [
       <div class="flex items-center md:order-2">
         <NavbarUserMenu />
 
-        <button data-collapse-toggle="navbar-user" type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-          aria-controls="navbar-user" aria-expanded="false">
-          <span class="sr-only">
-            Open main menu
-          </span>
-          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15" />
-          </svg>
-        </button>
+        <!-- Hamburger button - show only to not authenticated users on small screens  -->
+        <template v-if="!authStore.isAuthenticated">
+          <button @click="isHamburgerOpen = !isHamburgerOpen"
+            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-100 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200">
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15" />
+            </svg>
+          </button>
+        </template>
       </div>
 
       <template v-if="!authStore.isAuthenticated">
-        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
+        <!-- Navbar pages menu for not-authenticated users for large screens / toggled by hamburger for small screens-->
+        <div class="items-center justify-between w-full md:flex md:w-auto md:order-1"
+          :class="isHamburgerOpen ? 'flex' : 'hidden'">
           <ul
-            class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
+            class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-transparent w-full md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
             <li v-for="(item, index) in menu" :key="`menu-item-${index}`">
-              <RouterLink :to="item.route" :class="item.classes">
+              <RouterLink :to="item.route" :class="item.classes" @click="isHamburgerOpen = false;">
                 {{ item.title }}
               </RouterLink>
             </li>
