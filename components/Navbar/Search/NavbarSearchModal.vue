13b669d9fb946fa911fd363356fc3facc64a8d97
hazadus <hazadus7@gmail.com>
2023-08-24 12:32:18 +0300

    ui: Search modal transitions added.

--- components/Navbar/Search/NavbarSearchModal.vue
+++ components/Navbar/Search/NavbarSearchModal.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Dialog, DialogOverlay } from "@headlessui/vue";
+import { Dialog, DialogOverlay, TransitionRoot, TransitionChild } from "@headlessui/vue";
 
 defineProps({
   isOpen: {
@@ -14,15 +14,40 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <Dialog
-    class="fixed inset-0 z-50 flex justify-center items-start"
-    :open="isOpen"
-    :onClose="() => emit('closeModal', true)"
+  <TransitionRoot
+    as="template"
+    :show="isOpen"
   >
-    <DialogOverlay class="fixed inset-0 bg-black bg-opacity-70"></DialogOverlay>
+    <Dialog
+      class="fixed inset-0 z-50 flex justify-center items-start"
+      :open="isOpen"
+      @close="() => emit('closeModal', true)"
+    >
+      <TransitionChild
+        as="template"
+        enter="duration-200 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <DialogOverlay class="fixed inset-0 bg-black bg-opacity-70"></DialogOverlay>
+      </TransitionChild>
 
-    <div class="w-full max-w-2xl bg-white rounded-lg mx-4 max-h-[80vh] mt-[10vh] relative">
-      SearchModal
-    </div>
-  </Dialog>
+      <TransitionChild
+        as="template"
+        enter="duration-200 ease-out"
+        enter-from="opacity-0 scale-95"
+        enter-to="opacity-100 scale-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100 scale-100"
+        leave-to="opacity-0 scale-95"
+      >
+        <div class="w-full max-w-2xl bg-white rounded-lg mx-4 max-h-[80vh] mt-[10vh] relative">
+          SearchModal
+        </div>
+      </TransitionChild>
+    </Dialog>
+  </TransitionRoot>
 </template>
