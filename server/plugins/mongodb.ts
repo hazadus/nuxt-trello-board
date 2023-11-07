3c6f156c003cd718488cc3869911491712ece25f
hazadus <hazadus7@gmail.com>
2023-11-07 17:02:54 +0300

    fix: User schema not registered server error after restart.

--- server/plugins/mongodb.ts
+++ server/plugins/mongodb.ts
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
+import { UserModel } from "@/server/models/User";
 
-export default defineNitroPlugin((nitroApp) => {
+export default defineNitroPlugin(async (nitroApp) => {
   const { mongoDbAddress } = useRuntimeConfig();
 
   mongoose
     .connect(mongoDbAddress)
     .then(() => console.log(`🔌 Connected to MongoDB at "${mongoDbAddress}"`))
     .catch((e) => console.log(`❌ Can't connect to MongoDB at "${mongoDbAddress}"!`, "Reason:", e));
+
+    /**
+     * This is to avoid `Schema hasn't been registered for model "User".Use mongoose.model(name, schema)`
+     * error after server restart.
+     */
+    const users = await UserModel.find();
+    console.log(users);
 });
